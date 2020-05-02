b766e60ee343fcc17199667be5d748192582eb85
Mhel Medina <rommel.d.medina@gmail.com>
2020-05-02 22:41:50 +0800

    add multi item slider

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('.games-slider-wrapper').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+    });
+});
