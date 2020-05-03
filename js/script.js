c2bca498fbba459f88cd780d0314f82130648a49
Mhel Medina <rommel.d.medina@gmail.com>
2020-05-03 10:17:02 +0800

    add slick slider

--- js/script.js
+++ js/script.js
@@ -1,20 +1,55 @@
-$(document).ready(function() {
-    $('.games-slider-wrapper').slick({
+$(document).ready(() => {
+    $('.multiple-items-centered').slick({
         centerMode: true,
         centerPadding: '60px',
         slidesToShow: 3,
-        nextArrow: $('.next'),
-        prevArrow: $('.prev'),
+        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        arrows: true,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
         responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
           {
             breakpoint: 768,
             settings: {
               arrows: false,
               centerMode: true,
               centerPadding: '40px',
-              slidesToShow: 3
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1,
+              slidesToScroll: 1
             }
           },
           {
@@ -23,7 +58,8 @@ $(document).ready(function() {
               arrows: false,
               centerMode: true,
               centerPadding: '40px',
-              slidesToShow: 1
+              slidesToShow: 1,
+              slidesToScroll: 1
             }
           }
         ]
