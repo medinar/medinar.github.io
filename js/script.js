5340c5be677ddc11fd5fa2009f3aae16f5d17d50
medinar <rommel.d.medina@gmail.com>
2020-05-07 15:38:55 +0800

    add events slick slider at landing page

--- js/script.js
+++ js/script.js
@@ -76,4 +76,77 @@ $(document).ready(() => {
           }
         ]
     });
+    $('.events-slick-slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+        responsive: [
+          {
+            breakpoint: 1100,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 5,
+              slidesToScroll: 1,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 4,
+              slidesToScroll: 1,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    });
 });
